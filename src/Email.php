c752b1c7d42cf447d8b5d8dd32dde402c681d695
haas36u <haasmyriam@yahoo.com>
2018-02-16 10:24:40 +0100

    Fix coding style for Email class and add new php-cs-fixer in travis.yml

--- src/Email.php
+++ src/Email.php
@@ -1,10 +1,29 @@
 <?php
 declare(strict_types=1);
 
+/**
+ * Email class
+ *
+ * Check if email is valid
+ *
+ * @category Email
+ * @package  Email
+ * @author   Myriam <haasmyriam8@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://example.com/email
+ */
+
 final class Email
 {
     private $email;
-    private $toto;
+
+    /**
+    * Email builder
+    *
+    * @param string $email Email
+    *
+    * @return string  Email
+    */
     private function __construct(string $email)
     {
         $this->ensureIsValidEmail($email);
@@ -12,16 +31,35 @@ final class Email
         $this->email = $email;
     }
 
-    public static function fromString(string $email): self
+    /**
+     * Email from string
+     *
+     * @param string $email Email
+     *
+     * @return string  Email
+    */
+    public static function __fromString(string $email): self
     {
         return new self($email);
     }
 
+    /**
+    * Email to string
+    *
+    * @return string Email
+    */
     public function __toString(): string
     {
         return $this->email;
     }
 
+    /**
+     * Is valid email
+     *
+     * @param string $email Email
+     *
+     * @return string  If not valid
+    */
     private function ensureIsValidEmail(string $email): void
     {
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
